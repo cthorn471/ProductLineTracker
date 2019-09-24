891d86f9f1d8d672b2ad0b9daf2eb32404893c99
cthorn471 <cthorn3030@eagle.fgcu.edu>
2019-09-24 17:51:20 -0400

    Added all of the scene builder stuff for week 3. Still need to rename some labels, combo boxes and fix the appearance of the gui.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,18 @@
 
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button btnAddProduct;
+
+    @FXML
+    void display(ActionEvent event) {
+    System.out.println("hello console");
+    }
 
 }
