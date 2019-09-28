cca37211fe866103a6791126a51d2a943d2de9ea
cthorn471 <cthorn3030@eagle.fgcu.edu>
2019-09-28 19:50:13 -0400

    Complete all of sprint 1 specifications and ran analyze code which contained 1 error and 6 warnings that I was unable to resolve.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,18 +4,68 @@
 
 package sample;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
+
+
+
+/**
+ * @author Christopher Thorn
+ *The controller class holds all the values from the objects being used in the GUI.
+ * 9/28/2019
+ */
 public class Controller {
 
-    @FXML
-    private Button btnAddProduct;
+  @FXML
+  private TextField TextFieldProductName;
 
-    @FXML
-    void display(ActionEvent event) {
-    System.out.println("hello console");
-    }
+  @FXML
+  private TextField TextFieldManufacturer;
 
+  @FXML
+  private ChoiceBox<?> ChoiceBoxItemType;
+
+  @FXML
+  private Button btnAddProduct;
+
+  @FXML
+  private Label TestLabel;
+
+  @FXML
+  private ComboBox<String> ComboBoxChooseQuantity;
+
+  @FXML
+  private Button btnRecordProduction;
+
+  /**
+   * The display method executes a button click mouse event when the "Add Product" button on the Product Line tab is pushed.
+   * Once the button is pushed all information in Product Name and Manufacturer Text fields is saved.
+   * @param mouseEvent allows the button click action to occur.
+   */
+  public void display(javafx.scene.input.MouseEvent mouseEvent) {
+
+    String SQLStatement = "INSERT INTO Product(type, manufacturer, name) VALUES ('AUDIO', "
+        + "'"
+        + TextFieldManufacturer.getText()
+        + "', '"
+        + TextFieldProductName.getText()
+        + "')";
+
+    //System.out.print(SQLStatement);
+    //pass the sql statement from SQLStatement to  the method sqlExecute in Main.
+    Main.sqlExecute(SQLStatement);
+  }
+
+  /**
+   * The initialize method creates the numbered list for the comboBox from 1-10 and allows the user the make a selection and edit previous selections.
+   */
+  @FXML
+  private void initialize() {
+    ComboBoxChooseQuantity.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+    ComboBoxChooseQuantity.getSelectionModel().selectFirst();
+    ComboBoxChooseQuantity.setEditable(true);
+
+  }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,35 +1,86 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 /**
  * @author Christopher Thorn
+ * The main.java file holds information that works with the controller.java and the ProductLineDB. The file creates and initialize a working databsase
+ * along with execution sql statements to the database
+ * 9/28/2019
  */
 public class Main extends Application {
 
   /**
-   *The start method is the starting point of a JavaFX program. This start method sets the title.
-   *  @brief
+   * The Statement stmt is declared globally to be used in executing the SQL statement in the sqlExecute method.
+   */
+  private static Statement stmt;
+
+  /**
+   * The start method is the starting point of a JavaFX program. This start method sets the title.
    * @param primaryStage
    * @throws Exception any problem with the code.
+   * @brief
    */
+
   @Override
   public void start(Stage primaryStage) throws Exception {
+    //establish database connection
+    initializeDB();
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.setTitle("Product Line Tracker");
+    primaryStage.setScene(new Scene(root, 400, 300));
     primaryStage.show();
   }
 
   /**
-   * @brief
+   * The sqlExecute method takes a string SQLStatement as an argument an executes an sql statement from data that was obtained in the GUI
+   * product Name and Manufacturer Textfield.
+   * @param SQLStatement
+   */
+  public static void sqlExecute(String SQLStatement) {
+    try {
+      stmt.executeUpdate(SQLStatement);
+      System.out.println(SQLStatement);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  /**
+   * The initializeDB method creates a connection to the database and sets the credentials.
+   */
+  private void initializeDB() {
+
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/ProductLineDB";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+
+    try {
+      Class.forName(JDBC_DRIVER);
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      stmt = conn.createStatement();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }//end try catch
+  }//end method initializeDB
+
+  /**
+   * The main method is empty for now but will eventually contain information.
    * @param args
    */
-
   public static void main(String[] args) {
     launch(args);
   }
